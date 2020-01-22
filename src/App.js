fdd2f43149e099e5706c22385c7cf593246c2d6d
Akhil <akhil9tiet@gmail.com>
2020-01-22 00:35:35 -0800

    wip: add server log tables

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import Cards from './Components/Cards/Cards';
 import DadJokes from './Components/API_call/DadJokes';
 import TwoSum from './Components/TwoSum';
+import Report from './Components/ServerLogs/Report';
 import Paper from '@material-ui/core/Paper';
 
 function App() {
+	const logs = [
+		'[01/Aug/1995:00:54:59 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511',
+		'[01/Aug/1995:00:55:04 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 363',
+		'[01/Aug/1995:00:55:06 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 403 298',
+		'[01/Aug/1995:00:55:09 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635',
+		'[01/Aug/1995:00:55:18 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511',
+		'[01/Aug/1995:00:56:52 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 36'
+	];
+
 	return (
 		<React.Fragment>
 			<Paper>
@@ -15,6 +25,9 @@ function App() {
 			</Paper>
 			<Paper>
 				<DadJokes />
+			</Paper>
+			<Paper>
+				<Report logs={logs} />
 			</Paper>
 		</React.Fragment>
 	);
---
+++ src/Components/ServerLogs/Report.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+const Report = (props) => {
+	var count = 0;
+	const { logs } = props;
+
+	var resultTab = [];
+
+	logs.map((log) => {
+		const logArr = log.split(' ');
+		const reqMessage = logArr[2].substring(1);
+		const fileName = logArr[3];
+		const resCode = logArr[5];
+		const bytes = logArr[6];
+		if (!logArr.length === 6) {
+			return <h1>Error!! there is an error with: {log} </h1>;
+		} else {
+			if (reqMessage === 'GET') {
+				if (resCode[0] === '2') {
+					if (count < 10) {
+						count++;
+						return resultTab.push([fileName, bytes]);
+					}
+				}
+			}
+		}
+	});
+	return (
+		<React.Fragment>
+			<Table>
+				<thead>
+					<tr>
+						<th>Filename</th>
+						<th>Bytes</th>
+					</tr>
+				</thead>
+				<tbody>
+					{resultTab.map((row, i) => (
+						<tr key={i}>
+							<td>{row[0]}</td>
+							<td>{row[1]}</td>
+						</tr>
+					))}
+				</tbody>
+			</Table>
+		</React.Fragment>
+	);
+};
+
+export default Report;
