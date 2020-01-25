f30d146bfa933a787ce9ffe6e29cc0533cd7e1d6
Akhil <akhil9tiet@gmail.com>
2020-01-25 05:40:04 -0800

    wip: setting up app.js for routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React from 'react';
+import Home from './Components/Home';
 import Cards from './Components/Cards/Cards';
 import DadJokes from './Components/API_call/DadJokes';
 import SideDrawer from './Components/SideDrawer';
-
+// import Report from './Components/ServerLogs/Report';
 import { Switch, Route } from 'react-router-dom';
-import Report from './Components/ServerLogs/Report';
+
 import Paper from '@material-ui/core/Paper';
 
 function App() {
-	const logs = [
-		'[01/Aug/1995:00:54:59 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511',
-		'[01/Aug/1995:00:55:04 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 363',
-		'[01/Aug/1995:00:55:06 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 403 298',
-		'[01/Aug/1995:00:55:09 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635',
-		'[01/Aug/1995:00:55:18 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511',
-		'[01/Aug/1995:00:56:52 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 36'
-	];
+	// const logs = [
+	// 	'[01/Aug/1995:00:54:59 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511',
+	// 	'[01/Aug/1995:00:55:04 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 363',
+	// 	'[01/Aug/1995:00:55:06 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 403 298',
+	// 	'[01/Aug/1995:00:55:09 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635',
+	// 	'[01/Aug/1995:00:55:18 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511',
+	// 	'[01/Aug/1995:00:56:52 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 36'
+	// ];
 
 	return (
 		<React.Fragment>
 			<SideDrawer />
-			<Paper>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route path='/home' component={Home} />
+				{/* <Route exact path='/dad-jokes' component={DadJokes} />
+				<Route exact path='/cards' component={Cards}></Route> */}
+				{/* <Route exact path='/server-logs' render={(props) => <Report {...props} />}></Route> */}
+			</Switch>
+			{/* <Paper>
 				<Cards />
-			</Paper>
-			<Paper>
+			</Paper> */}
+			{/*<Paper>
 				<DadJokes />
 			</Paper>
 			<Paper>
 				<Report logs={logs} />
-			</Paper>
+			</Paper> */}
 
 			{/* <Paper>
 				<TwoMovies movies={[90, 86, 75, 60, 120, 150, 125]} duration={250} />
