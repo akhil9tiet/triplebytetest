896ad981cee3eb9fe2939ba293d82f332bc51cff
Akhil <akhil9tiet@gmail.com>
2020-03-19 23:45:51 -0700

    wip: traffic lights by interval

--- src/Components/TrafficLight/TrafficLight.js
+++ src/Components/TrafficLight/TrafficLight.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
 
 const TrafficLight = () => {
-	const [light, setLight] = useState(100);
-	var lightarr = ['red', 'black', 'black'];
+	const [lights, setLights] = useState({ light: ['red', 'black', 'black'], start: true });
 
-	if (light === 100) {
-		lightarr = ['red', 'black', 'black'];
-		setInterval(() => setLight(10), 2000);
-	} else if (light === 10) {
-		lightarr = ['black', 'yellow', 'black'];
-		setInterval(() => setLight(1), 2000);
-	} else {
-		lightarr = ['black', 'black', 'green'];
-		setInterval(() => setLight(100), 2000);
-	}
+	const lightsHandler = () => {
+		setLights({ ...lights, start: !lights.start });
 
+		if (lights.start) {
+			setInterval(() => setLights({ ...lights, light: ['black', 'yellow', 'black'] }), 1000);
+			setInterval(() => setLights({ ...lights, light: ['black', 'black', 'green'] }), 1000);
+			setInterval(() => setLights({ ...lights, light: ['red', 'black', 'black'] }), 1000);
+		} else {
+		}
+	};
 	return (
 		<React.Fragment>
-			<svg height='100' width='100'>
-				<circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill={lightarr[0]} />
-			</svg>
-			<svg height='100' width='100'>
-				<circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill={lightarr[1]} />
-			</svg>
-			<svg height='100' width='100'>
-				<circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill={lightarr[2]} />
-			</svg>
+			<div style={{ padding: 30 }}>
+				<svg height='100' width='100'>
+					<circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill={lights.light[0]} />
+				</svg>
+				<svg height='100' width='100'>
+					<circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill={lights.light[1]} />
+				</svg>
+				<svg height='100' width='100'>
+					<circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill={lights.light[2]} />
+				</svg>
+				<br />
+
+				<button style={{ padding: 10, backgroundColor: '#3e4555' }} onClick={lightsHandler}>
+					{lights.start ? 'Start' : 'End'}
+				</button>
+			</div>
 		</React.Fragment>
 	);
 };
