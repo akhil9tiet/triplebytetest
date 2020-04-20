7a270a741d0b3fd8250cd6e865801d12fdece4c3
Akhil <akhil9tiet@gmail.com>
2020-04-20 00:01:48 -0700

    fix: error in data

---
+++ src/Components/DeltaIsometric/DeltaIsometric.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DeltaIsometric = () => {
+	return (
+		<React.Fragment>
+			<h1>DeltaIsometric</h1>
+			<h1>DeltaIsometric</h1>
+			<h1>DeltaIsometric</h1>
+			<h1>DeltaIsometric</h1>
+			<h1>DeltaIsometric</h1>
+		</React.Fragment>
+	);
+};
+
+export default DeltaIsometric;
