403770c86f39d209616fcd2c05030ce29a421edd
Akhil <akhil9tiet@gmail.com>
2020-04-24 21:11:33 -0700

    wip: add dynamic button text

--- src/Components/Stepper/StepperComponent.js
+++ src/Components/Stepper/StepperComponent.js
@@ -4,7 +4,8 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import {Consent, ChooseMethod, VerifyCode } from './Comps';
+import { Consent, ChooseMethod, VerifyCode } from './Comps';
+import Grid from '@material-ui/core/Grid';
 
 function getSteps() {
 	return ['Consent', 'Verification Method', 'Verify Code'];
@@ -20,6 +21,19 @@ function getStepContent(stepIndex) {
 			return <VerifyCode />;
 		default:
 			return 'Unknown stepIndex';
+	}
+}
+
+function buttonName(stepIndex) {
+	switch (stepIndex) {
+		case 0:
+			return 'Allow';
+		case 1:
+			return 'Next';
+		case 2:
+			return 'Submit';
+		default:
+			return 'Error';
 	}
 }
 
@@ -40,34 +54,38 @@ const StepperComponent = () => {
 	};
 
 	return (
-		<div>
-			<Stepper activeStep={activeStep} alternativeLabel>
-				{steps.map((label) => (
-					<Step key={label}>
-						<StepLabel>{label}</StepLabel>
-					</Step>
-				))}
-			</Stepper>
-			<div>
-				{activeStep === steps.length ? (
-					<div>
-						<Typography>All steps completed</Typography>
-						<Button onClick={handleReset}>Reset</Button>
-					</div>
-				) : (
-					<div>
-						<Typography>{getStepContent(activeStep)}</Typography>
+		<div style={{ paddingLeft: 150 }}>
+			<Grid container spacing={3}>
+				<Grid item xs={12}>
+					<Stepper activeStep={activeStep} alternativeLabel>
+						{steps.map((label) => (
+							<Step key={label}>
+								<StepLabel>{label}</StepLabel>
+							</Step>
+						))}
+					</Stepper>
+				</Grid>
+				<Grid item xs={12} alignContent='center'>
+					{activeStep === steps.length ? (
 						<div>
-							<Button disabled={activeStep === 0} onClick={handleBack}>
-								Back
-							</Button>
-							<Button variant='contained' color='primary' onClick={handleNext}>
-								{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-							</Button>
+							<Typography>All steps completed</Typography>
+							<Button onClick={handleReset}>Reset</Button>
 						</div>
+					) : (
+						<div>{getStepContent(activeStep)}</div>
+					)}
+				</Grid>
+				<Grid item xs={12}>
+					<div>
+						<Button disabled={activeStep === 0 || activeStep === 1} onClick={handleBack}>
+							Back
+						</Button>
+						<Button variant='contained' color='primary' onClick={handleNext}>
+							{buttonName(activeStep)}
+						</Button>
 					</div>
-				)}
-			</div>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
