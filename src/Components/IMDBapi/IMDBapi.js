96ef240157a63d7bf5440691e088d01d3f87f70f
Akhil <akhil9tiet@gmail.com>
2020-05-19 00:19:32 -0700

    fix: bottom axis

--- src/Components/IMDBapi/IMDBapi.js
+++ src/Components/IMDBapi/IMDBapi.js
@@ -1,76 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import imdb, { Client } from 'imdb-api';
-import HeatmapChart from './HeatmapChart';
+import React, { useEffect, useState } from "react";
+import imdb, { Client } from "imdb-api";
+import HeatmapChart from "./HeatmapChart";
 
 const IMDBapi = () => {
-	const [data, setData] = useState(null);
-	var seriesName = 'Mad Men';
-	useEffect(() => {
-		// const cli = new Client({ apiKey: '81e1b710' });
-		const cli = new Client({ apiKey: 'd19ea01b' });
+  const [data, setData] = useState(null);
+  var seriesName = "Breaking Bad";
+  useEffect(() => {
+    // const cli = new Client({ apiKey: '81e1b710' });
+    const cli = new Client({ apiKey: "d19ea01b" });
 
-		cli.get({ name: seriesName })
-			.then((things) => {
-				return things.episodes();
-			})
-			.then((eps) => {
-				// setData(eps);
-				setData(eps.filter((el) => el.name !== undefined));
-			});
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+    cli
+      .get({ name: seriesName })
+      .then(things => {
+        return things.episodes();
+      })
+      .then(eps => {
+        // setData(eps);
+        setData(eps.filter(el => el.name !== undefined));
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	console.log('@@IMDB API DATA', JSON.stringify(data));
+  console.log("@@IMDB API DATA", JSON.stringify(data));
 
-	const newDataFormatter = (d) => {
-		var newData = [];
-		var seasonsArr = [];
+  const newDataFormatter = d => {
+    var newData = [];
+    var seasonsArr = [];
 
-		d.map((el) => seasonsArr.push(el['season']));
+    d.map(el => seasonsArr.push(el["season"]));
 
-		var seasonsArrUnique = [...new Set(seasonsArr)]; //remove duplicates
-		seasonsArrUnique.forEach((season) =>
-			// newData.push({ season: season, episodes: data.filter((e) => e.season === season) })
-			newData.push({
-				bin: season,
-				/*****************************************************
-				 * The code lines from 45 to 50 have been with the help of this great person from Reddit u/GreenSnow02.
-				 * https://www.reddit.com/r/reactjs/comments/gjzmlp/data_formatting_question/
-				 ******************************************************/
-				bins: d.reduce((accum, e) => {
-					if (e.season === season) {
-						accum.push({
-							bin: e.episode * 150,
-							count: e.rating,
-							//todo
-							// name:,
-							// dateOfRelease:,
-						});
-					}
-					return accum;
-				}, []), // [] set accum as an empty array to begin with
-				/***************************************************** */
-			})
-		);
-		// var filteredData = newData.map((el) => {...el, bins:[el.bins.filter((e) => e)]	x});
-		// return filteredData;
-		return newData;
-	};
+    var seasonsArrUnique = [...new Set(seasonsArr)]; //remove duplicates
+    seasonsArrUnique.forEach(season =>
+      // newData.push({ season: season, episodes: data.filter((e) => e.season === season) })
+      newData.push({
+        bin: season,
+        /*****************************************************
+         * The code lines from 45 to 50 have been with the help of this great person from Reddit u/GreenSnow02.
+         * https://www.reddit.com/r/reactjs/comments/gjzmlp/data_formatting_question/
+         ******************************************************/
+        bins: d.reduce((accum, e) => {
+          if (e.season === season) {
+            accum.push({
+              bin: e.episode * 150,
+              count: e.rating
+              //todo
+              // name:,
+              // dateOfRelease:,
+            });
+          }
+          return accum;
+        }, []) // [] set accum as an empty array to begin with
+        /***************************************************** */
+      })
+    );
+    // var filteredData = newData.map((el) => {...el, bins:[el.bins.filter((e) => e)]	x});
+    // return filteredData;
+    return newData;
+  };
 
-	// console.log('@@newData', newDataFormatter(data));
+  // console.log('@@newData', newDataFormatter(data));
 
-	if (data === null) {
-		return <p>Loading ...</p>;
-	}
-	// return <p>loadfing</p>;
-	return (
-		<React.Fragment>
-			{/* <div style={{ height: 'initial', 'text-align': 'center' }}> */}
-			<h2>{seriesName}</h2>
-			<HeatmapChart data={newDataFormatter(data)} />
-			{/* </div> */}
-		</React.Fragment>
-	);
+  if (data === null) {
+    return <p>Loading ...</p>;
+  }
+  // return <p>loadfing</p>;
+  return (
+    <React.Fragment>
+      {/* <div style={{ height: 'initial', 'text-align': 'center' }}> */}
+      <h2>{seriesName}</h2>
+      <HeatmapChart data={newDataFormatter(data)} />
+      {/* </div> */}
+    </React.Fragment>
+  );
 };
 
 export default IMDBapi;
